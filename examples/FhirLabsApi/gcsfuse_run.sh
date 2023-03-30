9c0430f850da6e2a5e3366d8c59c05eaf84e5bc5
Joseph Shook <Joseph.Shook@Surescripts.com>
2023-03-29 17:43:32 -0700

    Update gcsfuse_run.sh

--- examples/FhirLabsApi/gcsfuse_run.sh
+++ examples/FhirLabsApi/gcsfuse_run.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 # Create mount directory for service
 mkdir -p $MNT_DIR
- 
+
 echo "Mounting GCS Fuse."
 gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
 echo "Mounting completed."

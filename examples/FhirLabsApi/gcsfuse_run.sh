44113033d100899fb85b4ac779c514146cd52f2f
Joseph Shook <Joseph.Shook@Surescripts.com>
2023-03-29 17:41:44 -0700

    Enable gcsfuse
    This enables the second process called gcsfuse to run.  gcsfuse is a user-space file system for interacting with Google Cloud Storage.  So basically a mount point is mapped to a Bucket in Google Cloud.  This saves a lot o money.  The alternative FileStore solution running with the least resources started at a flat $204/month.  Remember that Fhirlabs is just a basic FHIR server using the files system as the database.

--- examples/FhirLabsApi/gcsfuse_run.sh
+++ examples/FhirLabsApi/gcsfuse_run.sh
@@ -1,11 +1,12 @@
-﻿#!/usr/bin/env bash
+#!/bin/bash
 set -eo pipefail
 
 # Create mount directory for service
 mkdir -p $MNT_DIR
-
+ 
 echo "Mounting GCS Fuse."
 gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
+touch /mnt/gcs/joe.txt
 echo "Mounting completed."
 
 # Start the application

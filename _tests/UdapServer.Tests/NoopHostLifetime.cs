2eed40e69df86314ba9f7ae32cfd8392594a99bd
Joseph Shook <Joseph.Shook@Surescripts.com>
2023-05-01 12:49:52 -0700

    Continue to experience intermittent failures on Github.
    Receiving this error
    "System.AggregateException : One or more hosted services failed to stop. (Not started. Call Start first"
    From tests using WebApplicationFactory .
    Info here: https://github.com/dotnet/aspnetcore/issues/38649
    And possible solution here:  https://github.com/dotnet/aspnetcore/pull/23761/files

---
+++ _tests/UdapServer.Tests/NoopHostLifetime.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Hosting;
+
+namespace UdapServer.Tests;
+internal class NoopHostLifetime : IHostLifetime
+{
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task WaitForStartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}

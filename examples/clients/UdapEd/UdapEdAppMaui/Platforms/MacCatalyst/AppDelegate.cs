542bdabf2ba214cb353b473772018883c5f5eb23
Joseph Shook <Joseph.Shook@Surescripts.com>
2024-02-24 07:32:01 -0800

    Remove UdapEd.  Moving to UdapEd repo

--- examples/clients/UdapEd/UdapEdAppMaui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace UdapEdAppMaui;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}

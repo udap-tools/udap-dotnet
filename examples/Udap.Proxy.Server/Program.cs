90281aa853ecd87538fedfd41599aa609c2d938b
Joseph Shook <Joseph.Shook@Surescripts.com>
2024-05-01 10:42:16 -0700

    Proxy Server additions
    Rewrite url references in FHIR resources
    
    Add Security section to capability statement.  May need to more work and packaging here but this is a POC at the moment.

--- examples/Udap.Proxy.Server/Program.cs
+++ examples/Udap.Proxy.Server/Program.cs
@@ -7,6 +7,12 @@ using Udap.Proxy.Server;
 using Yarp.ReverseProxy.Transforms;
 using Google.Apis.Auth.OAuth2;
 using Udap.Smart.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Serialization;
+using Hl7.Fhir.Model;
+using System;
+using ZiggyCreatures.Caching.Fusion;
+using Task = System.Threading.Tasks.Task;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +25,14 @@ builder.Services.AddControllersWithViews();
 builder.Services.Configure<SmartMetadata>(builder.Configuration.GetRequiredSection("SmartMetadata"));
 builder.Services.AddSmartMetadata();
 builder.Services.AddUdapMetadataServer(builder.Configuration);
+builder.Services.AddFusionCache()
+    .WithDefaultEntryOptions(new FusionCacheEntryOptions
+    {
+        Duration = TimeSpan.FromMinutes(10),
+        FactorySoftTimeout = TimeSpan.FromMilliseconds(100),
+        AllowTimedOutFactoryBackgroundCompletion = true,
+        FailSafeMaxDuration = TimeSpan.FromHours(12)
+    });
 
 builder.Services.AddAuthentication(OidcConstants.AuthenticationSchemes.AuthorizationHeaderBearer)
 
@@ -26,7 +40,7 @@ builder.Services.AddAuthentication(OidcConstants.AuthenticationSchemes.Authoriza
     {
         options.Authority = builder.Configuration["Jwt:Authority"];
         options.RequireHttpsMetadata = bool.Parse(builder.Configuration["Jwt:RequireHttpsMetadata"] ?? "true");
-        
+
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateAudience = false
@@ -45,12 +59,12 @@ builder.Services.AddReverseProxy()
     .ConfigureHttpClient((context, handler) =>
     {
         // this is required to decompress automatically.  *******   troubleshooting only   *******
-        handler.AutomaticDecompression = System.Net.DecompressionMethods.All; 
+        handler.AutomaticDecompression = System.Net.DecompressionMethods.All;
     })
     .AddTransforms(builderContext =>
     {
         // Conditionally add a transform for routes that require auth.
-        if (builderContext.Route.Metadata != null && 
+        if (builderContext.Route.Metadata != null &&
             (builderContext.Route.Metadata.ContainsKey("GCPKeyResolve") || builderContext.Route.Metadata.ContainsKey("AccessToken")))
         {
             builderContext.AddRequestTransform(async context =>
@@ -66,7 +80,7 @@ builder.Services.AddReverseProxy()
         // Use the default credentials.  Primary usage: running in Cloud Run under a specific service account
         if (builderContext.Route.Metadata != null && (builderContext.Route.Metadata.TryGetValue("ADC", out string? adc)))
         {
-            if(adc == "True")
+            if (adc == "True")
             {
                 builderContext.AddRequestTransform(async context =>
                 {
@@ -76,6 +90,39 @@ builder.Services.AddReverseProxy()
                 });
             }
         }
+
+        builderContext.AddResponseTransform(async responseContext =>
+        {
+            if (responseContext.HttpContext.Request.Path == "/fhir/r4/metadata")
+            {
+                responseContext.SuppressResponseBody = true;
+                var cache = responseContext.HttpContext.RequestServices.GetRequiredService<IFusionCache>();
+                var bytes = await cache.GetOrSetAsync("metadata", _ => GetFhirMetadata(responseContext, builder));
+
+                // Change Content-Length to match the modified body, or remove it.
+                responseContext.HttpContext.Response.ContentLength = bytes?.Length;
+                // Response headers are copied before transforms are invoked, update any needed headers on the HttpContext.Response.
+                await responseContext.HttpContext.Response.Body.WriteAsync(bytes);
+            }
+        });
+
+        builderContext.AddResponseTransform(async responseContext =>
+        {
+            if (responseContext.HttpContext.Request.Path != "/fhir/r4/metadata" && responseContext.HttpContext.Request.Path.StartsWithSegments("/fhir/r4/"));
+            {
+                var stream = await responseContext.ProxyResponse!.Content.ReadAsStreamAsync();
+                using var reader = new StreamReader(stream);
+                // TODO: size limits, timeouts
+                var body = await reader.ReadToEndAsync();
+                responseContext.SuppressResponseBody = true;
+
+                var finalBytes = Encoding.UTF8.GetBytes(body.Replace($"\"url\": \"{builder.Configuration["FhirUrlProxy:Back"]}",
+                    $"\"url\": \"{builder.Configuration["FhirUrlProxy:Front"]}"));
+                responseContext.HttpContext.Response.ContentLength = finalBytes.Length;
+
+                await responseContext.HttpContext.Response.Body.WriteAsync(finalBytes);
+            }
+        });
     });
 
 var app = builder.Build();
@@ -92,7 +139,7 @@ app.UseAuthorization();
 app.MapReverseProxy();
 
 app.UseSmartMetadata();
-app.UseUdapMetadataServer();
+app.UseUdapMetadataServer("fhir/r4"); // Ensure metadata can only be called from this base URL.
 
 app.Run();
 
@@ -123,7 +170,7 @@ async Task<string?> ResolveAccessToken(IReadOnlyDictionary<string, string> metad
     catch (Exception ex)
     {
         Console.WriteLine(ex); //todo: Logger
-        
+
         return string.Empty;
     }
 
@@ -133,4 +180,42 @@ async Task<string?> ResolveAccessToken(IReadOnlyDictionary<string, string> metad
 async Task<string> UdapMedatData(string s)
 {
     return s;
+}
+
+async Task<byte[]?> GetFhirMetadata(ResponseTransformContext responseTransformContext,
+    WebApplicationBuilder webApplicationBuilder)
+{
+    var stream = await responseTransformContext.ProxyResponse.Content.ReadAsStreamAsync();
+    using var reader = new StreamReader(stream);
+    var body = await reader.ReadToEndAsync();
+
+    if (!string.IsNullOrEmpty(body))
+    {
+        var capStatement = await new FhirJsonParser().ParseAsync<CapabilityStatement>(body);
+        var securityComponent = new CapabilityStatement.SecurityComponent();
+
+        securityComponent.Service.Add(
+            new CodeableConcept("http://fhir.udap.org/CodeSystem/capability-rest-security-service",
+                "UDAP",
+                "OAuth2 using UDAP profile (see http://www.udap.org)"));
+
+        //
+        // https://build.fhir.org/ig/HL7/fhir-extensions/StructureDefinition-oauth-uris.html
+        //
+        var oauthUrlExtensions = new Extension();
+        var securityExtension = new Extension("http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris", oauthUrlExtensions);
+        securityExtension.Extension.Add(new Extension() { Url = "token", Value = new FhirUri(webApplicationBuilder.Configuration["Jwt:Token"]) });
+        securityExtension.Extension.Add(new Extension() { Url = "authorize", Value = new FhirUri(webApplicationBuilder.Configuration["Jwt:Authorize"]) });
+        securityExtension.Extension.Add(new Extension() { Url = "register", Value = new FhirUri(webApplicationBuilder.Configuration["Jwt:Register"]) });
+        securityExtension.Extension.Add(new Extension() { Url = "manage", Value = new FhirUri(webApplicationBuilder.Configuration["Jwt:Manage"]) });
+        securityComponent.Extension.Add(securityExtension);
+        capStatement.Rest.First().Security = securityComponent;
+
+        body = new FhirJsonSerializer().SerializeToString(capStatement);
+        var bytes = Encoding.UTF8.GetBytes(body);
+        
+        return bytes;
+    }
+
+    return null;
 }

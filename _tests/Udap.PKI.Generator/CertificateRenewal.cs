662ef64ba08a6fe3b0040b5f5cfb0808eabc71d8
Joseph Shook <JoeShook@Gmail.com>
2024-06-20 07:34:18 -0700

    Save some experimetal work on renewing certificates

---
+++ _tests/Udap.PKI.Generator/CertificateRenewal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Udap.PKI.Generator;
+public class CertificateRenewal : CertificateBase
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+
+    //
+    // Community:SureFhirLabs:: Certificate Store File Constants
+    //
+    private static string SureFhirLabsCertStore
+    {
+        get
+        {
+            var baseDir = BaseDir;
+
+            return $"{baseDir}/certstores/surefhirlabs_community";
+        }
+    }
+
+    private static string SurefhirlabsUdapIntermediates { get; } = $"{SureFhirLabsCertStore}/intermediates";
+    private static string SurefhirlabsUdapIntermediatesRenewed { get; } = $"{SureFhirLabsCertStore}/intermediates/renewed";
+
+    [Fact]
+    public void RenewIntermediateCertificate()
+    {
+        using var rootCA = new X509Certificate2($"{SureFhirLabsCertStore}/SureFhirLabs_CA.pfx", "udap-test");
+        using var subCA = new X509Certificate2($"{SurefhirlabsUdapIntermediates}/SureFhirLabs_Intermediate.pfx", "udap-test");
+
+
+
+        //SurefhirlabsUdapIntermediatesRenewed
+    }
+}

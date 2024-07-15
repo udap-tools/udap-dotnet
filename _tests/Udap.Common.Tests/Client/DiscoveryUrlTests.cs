0c2889da96290bf7c298c516a9696f5f8f19b88a
Joseph Shook <JoeShook@Gmail.com>
2024-07-15 08:51:04 -0700

    DiscoveryUrl Parsing fixup
    Allow the consumer to pass a fully build UDAP well-known url with community parameter and handle it.

---
+++ _tests/Udap.Common.Tests/Client/DiscoveryUrlTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Udap.Client.Client;
+
+namespace Udap.Common.Tests.Client;
+public class DiscoveryUrlTests
+{
+    [Fact]
+    public void TestCommunityParsingWithFullCommunityUrl()
+    {
+        var result = DiscoveryEndpoint.ParseUrl("https://fhirlabs.net/fhir/r4/.well-known/udap?community=udap://fhirlabs.net/");
+
+        result.Url.Should().Be("https://fhirlabs.net/fhir/r4/.well-known/udap?community=udap://fhirlabs.net/");
+        result.Authority.Should().Be("https://fhirlabs.net/fhir/r4");
+    }
+
+    [Fact]
+    public void TestCommunityParsingWithCommunityParam()
+    {
+        var result = DiscoveryEndpoint.ParseUrl("https://fhirlabs.net/fhir/r4", null, "udap://fhirlabs.net/");
+
+        result.Url.Should().Be("https://fhirlabs.net/fhir/r4/.well-known/udap?community=udap://fhirlabs.net/");
+        result.Authority.Should().Be("https://fhirlabs.net/fhir/r4");
+    }
+}

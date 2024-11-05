f42c2799ba4048b8080a801f31f598b5574d64a2
Joseph Shook <JoeShook@Gmail.com>
2024-11-05 15:29:25 -0800

    Working on the ability to terminate trust at a chosen Trust Anchor.

--- Udap.Common/Certificates/TrustChainValidator.cs
+++ Udap.Common/Certificates/TrustChainValidator.cs
@@ -69,10 +69,7 @@ namespace Udap.Common.Certificates
                    X509ChainStatusFlags.InvalidBasicConstraints |
                    X509ChainStatusFlags.CtlNotTimeValid |
                    X509ChainStatusFlags.OfflineRevocation |
-                   X509ChainStatusFlags.CtlNotSignatureValid |
-                   X509ChainStatusFlags.RevocationStatusUnknown | // can't trust the chain to check revocation.
-                   X509ChainStatusFlags.PartialChain |
-                   X509ChainStatusFlags.UntrustedRoot;
+                   X509ChainStatusFlags.CtlNotSignatureValid;
         }
 
         /// <summary>
@@ -81,7 +78,11 @@ namespace Udap.Common.Certificates
         public TrustChainValidator(ILogger<TrustChainValidator> logger)
             : this(new X509ChainPolicy(), BuildDefaultProblemFlags(), logger)
         {
-            _validationPolicy.VerificationFlags = X509VerificationFlags.IgnoreWrongUsage;
+            _validationPolicy.VerificationFlags = X509VerificationFlags.IgnoreCertificateAuthorityRevocationUnknown |
+                                                  X509VerificationFlags.IgnoreEndRevocationUnknown |
+                                                  X509VerificationFlags.AllowUnknownCertificateAuthority |
+                                                  X509VerificationFlags.IgnoreWrongUsage;
+
             _validationPolicy.RevocationFlag = DefaultX509RevocationFlag;
             _validationPolicy.RevocationMode = DefaultX509RevocationMode;
         }
@@ -134,7 +135,7 @@ namespace Udap.Common.Certificates
             chainElements = null;
 
             // Let's avoid complex state and/or race conditions by making copies of these collections.
-            var roots = new X509Certificate2Collection(anchorCertificates); 
+            var roots = new X509Certificate2Collection(anchorCertificates);
             X509Certificate2Collection? intermediatesCloned = null;
 
             if (intermediateCertificates != null)
@@ -144,7 +145,7 @@ namespace Udap.Common.Certificates
 
             // ReSharper disable once RedundantAssignment
             intermediateCertificates = null;
-            
+
 
             // if there are no anchors we should always fail
             if (roots.IsNullOrEmpty())
@@ -183,7 +184,7 @@ namespace Udap.Common.Certificates
                 {
                     chainBuilder.ChainPolicy.ExtraStore.AddRange(intermediatesCloned);
                 }
-                var result = chainBuilder.Build(certificate);
+                var passedChainBuild = chainBuilder.Build(certificate);
 
                 // We're using the system class as a helper to build the chain
                 // However, we will review each item in the chain ourselves, because we have our own rules...
@@ -205,7 +206,7 @@ namespace Udap.Common.Certificates
 
                     if (this.ChainElementHasProblems(chainElement))
                     {
-                        this.NotifyProblem(chainElement);                        
+                        this.NotifyProblem(chainElement);
                         this.NotifyUntrusted(chainElement.Certificate);
                         return false;
                     }
@@ -221,19 +222,28 @@ namespace Udap.Common.Certificates
                         {
                             communityId = anchorList.First(a => a.Thumbprint == chainElement.Certificate.Thumbprint).CommunityId;
                         }
-
-                        continue;
                     }
 
-                    if (this.ChainElementHasProblems(chainElement))
+                    if (!passedChainBuild && this.ChainElementHasProblems(chainElement))
                     {
+                        // chain statuses can still be subscribed too.  There may be data to share with the consumer
+                        // that do not mean the chain is invalid.  passedChainBuild is the final arbiter of trust
+                        // for a x509Chain.
                         this.NotifyProblem(chainElement);
-                        this.NotifyUntrusted(chainElement.Certificate);
-                        return false;
+
+                        if (!passedChainBuild)
+                        {
+                            this.NotifyUntrusted(chainElement.Certificate);
+                        }
+
+                        if (passedChainBuild && foundAnchor)
+                        {
+                            return true;
+                        }
                     }
                 }
 
-                if (foundAnchor && !result)
+                if (foundAnchor && !passedChainBuild)
                 {
                     //
                     // Can end up here if problem flags exist that we do not care about.
@@ -249,7 +259,7 @@ namespace Udap.Common.Certificates
                     this.NotifyUntrusted(certificate);
                 }
 
-                return foundAnchor;  
+                return passedChainBuild;
             }
             catch (Exception ex)
             {
@@ -258,6 +268,7 @@ namespace Udap.Common.Certificates
             }
 
             this.NotifyUntrusted(certificate);
+
             return false;
         }
 
---
+++ _tests/Udap.Common.Tests/TerminateAtAnchorTest.cs
@@ -0,0 +1,160 @@
+﻿#region (c) 2024 Joseph Shook. All rights reserved.
+// /*
+//  Authors:
+//     Joseph Shook   Joseph.Shook@Surescripts.com
+// 
+//  See LICENSE in the project root for license information.
+// */
+#endregion
+
+using System.Security.Cryptography.X509Certificates;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Udap.Common.Certificates;
+
+namespace Udap.Common.Tests;
+public class TerminateAtAnchorTest
+{
+    private X509Certificate2 cert;
+    private X509Certificate2 anchor;
+
+    public TerminateAtAnchorTest()
+    {
+        cert = new X509Certificate2(Path.Combine("CertStore/issued", "fhirlabs.net.client.pfx"), "udap-test");
+        anchor = new X509Certificate2(Path.Combine("CertStore/intermediates", "SureFhirLabs_Intermediate.cer"));
+    }
+    [Fact]
+    public void TestAnchorTermination()
+    {
+        var logger = CreateLogger();
+        var chainPolicy = new X509ChainPolicy
+        {
+            TrustMode = X509ChainTrustMode.CustomRootTrust,
+            RevocationMode = X509RevocationMode.Online,
+            VerificationFlags = X509VerificationFlags.IgnoreCertificateAuthorityRevocationUnknown |
+                                X509VerificationFlags.IgnoreEndRevocationUnknown |
+                                X509VerificationFlags.AllowUnknownCertificateAuthority
+        };
+
+        var validator = new TrustChainValidator(chainPolicy, logger);
+        var diagnosticsChainValidator = SetupDiagnostics(validator);
+
+        var anchors = new X509Certificate2Collection { anchor };
+
+        var result = validator.IsTrustedCertificate("client_name", cert, null, anchors);
+        result.Should().BeTrue(
+            string.Join("\r\n", diagnosticsChainValidator.ActualProblemMessages)
+            + "\r\n" + string.Join("\r\n", diagnosticsChainValidator.ActualErrorMessages)
+            + "\r\n" + string.Join("\r\n", diagnosticsChainValidator.ActualUntrustedMessages));
+
+        diagnosticsChainValidator.ActualErrorMessages.Count.Should().Be(0);
+        diagnosticsChainValidator.ActualProblemMessages.Count.Should().Be(0);
+        diagnosticsChainValidator.ActualUntrustedMessages.Count.Should().Be(0);
+    }
+
+    [Theory]
+    [InlineData(X509VerificationFlags.IgnoreWrongUsage)]
+    [InlineData(X509VerificationFlags.IgnoreCertificateAuthorityRevocationUnknown)]
+    [InlineData(X509VerificationFlags.IgnoreEndRevocationUnknown)]
+    [InlineData(X509VerificationFlags.AllowUnknownCertificateAuthority)]
+    [InlineData(X509VerificationFlags.IgnoreCertificateAuthorityRevocationUnknown |
+                X509VerificationFlags.IgnoreEndRevocationUnknown)]
+    [InlineData(X509VerificationFlags.IgnoreEndRevocationUnknown |
+                X509VerificationFlags.AllowUnknownCertificateAuthority)]
+    [InlineData(X509VerificationFlags.IgnoreCertificateAuthorityRevocationUnknown |
+                X509VerificationFlags.AllowUnknownCertificateAuthority)]
+    public void TestAnchorTermination_Fail(X509VerificationFlags verificationFlags)
+    {
+        var logger = CreateLogger();
+        var chainPolicy = new X509ChainPolicy
+        {
+            TrustMode = X509ChainTrustMode.CustomRootTrust,
+            RevocationMode = X509RevocationMode.Online,
+            VerificationFlags = verificationFlags
+        };
+
+        var validator = new TrustChainValidator(chainPolicy, logger);
+        var diagnosticsChainValidator = SetupDiagnostics(validator);
+
+        X509Certificate2Collection anchors = new X509Certificate2Collection { anchor };
+
+        var result = validator.IsTrustedCertificate("client_name", cert, null, anchors);
+        result.Should().BeFalse(
+            string.Join("\r\n", diagnosticsChainValidator.ActualProblemMessages)
+            + "\r\n" + string.Join("\r\n", diagnosticsChainValidator.ActualErrorMessages)
+            + "\r\n" + string.Join("\r\n", diagnosticsChainValidator.ActualUntrustedMessages));
+
+        diagnosticsChainValidator.ActualErrorMessages.Count.Should().Be(0);
+        diagnosticsChainValidator.ActualProblemMessages.Count.Should().Be(4);
+        diagnosticsChainValidator.ActualUntrustedMessages.Count.Should().Be(2);
+    }
+
+    
+    private static ILogger<TrustChainValidator> CreateLogger()
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole(options =>
+            {
+                options.LogToStandardErrorThreshold = LogLevel.Trace;
+            });
+            builder.SetMinimumLevel(LogLevel.Trace);
+        });
+
+        return loggerFactory.CreateLogger<TrustChainValidator>();
+    }
+
+    private static FakeChainValidatorDiagnostics SetupDiagnostics(TrustChainValidator validator)
+    {
+        var diagnosticsChainValidator = new FakeChainValidatorDiagnostics();
+        validator.Problem += diagnosticsChainValidator.OnChainProblem;
+        validator.Error += diagnosticsChainValidator.OnCertificateError;
+        validator.Untrusted += diagnosticsChainValidator.OnUntrusted;
+        return diagnosticsChainValidator;
+    }
+}
+
+public class FakeChainValidatorDiagnostics
+{
+    public bool Called;
+
+    private readonly List<string> _actualProblemMessages = new List<string>();
+    private readonly List<string> _actualErrorMessages = new List<string>();
+    private readonly List<string> _actualUntrustedMessages = new List<string>();
+
+    public List<string> ActualProblemMessages
+    {
+        get { return _actualProblemMessages; }
+    }
+
+    public List<string> ActualErrorMessages
+    {
+        get { return _actualErrorMessages; }
+    }
+
+    public List<string> ActualUntrustedMessages
+    {
+        get { return _actualUntrustedMessages; }
+    }
+
+    public void OnChainProblem(X509ChainElement chainElement)
+    {
+        foreach (var chainElementStatus in chainElement.ChainElementStatus)
+        {
+            var problem = $"Trust ERROR {chainElementStatus.StatusInformation}, {chainElement.Certificate}";
+            _actualProblemMessages.Add(problem);
+        }
+    }
+
+    public void OnCertificateError(X509Certificate2 certificate, Exception error)
+    {
+        _actualErrorMessages.Add(error.Message);
+        //Logger.Error("RESOLVER ERROR {0}, {1}", resolver.GetType().Name, error.Message);
+    }
+
+    public void OnUntrusted(X509Certificate2 certificate)
+    {
+        _actualUntrustedMessages.Add($"\r\n Untrusted Certificate: {certificate}");
+        //Logger.Error("RESOLVER ERROR {0}, {1}", resolver.GetType().Name, error.Message);
+    }
+}
